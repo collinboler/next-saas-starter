866328aaccae32b9cafd95907054595597d0ccad
Collin Boler <cb6256@princeton.edu>
2024-11-30 21:20:49 -0500

    import analytics for vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata, Viewport } from 'next';
 import { Manrope } from 'next/font/google';
 import { UserProvider } from '@/lib/auth';
 import { getUser } from '@/lib/db/queries';
-
+import { Analytics } from "@vercel/analytics/react"
 export const metadata: Metadata = {
   title: 'Next.js SaaS Starter',
   description: 'Get started quickly with Next.js, Postgres, and Stripe.',
@@ -28,7 +28,7 @@ export default function RootLayout({
       className={`bg-white dark:bg-gray-950 text-black dark:text-white ${manrope.className}`}
     >
       <body className="min-h-[100dvh] bg-gray-50">
-        <UserProvider userPromise={userPromise}>{children}</UserProvider>
+        <UserProvider userPromise={getUser() as Promise<any>}>{children}</UserProvider>
       </body>
     </html>
   );
