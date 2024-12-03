ac9b7b4cc8447f5b5f30e1a906d9f93937bc0eb1
Collin Boler <cb6256@princeton.edu>
2024-12-03 05:05:28 -0500

    no footer

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -118,7 +118,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <section className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow">{children}</main>
-      <Footer />
+ 
     </section>
   );
 }
