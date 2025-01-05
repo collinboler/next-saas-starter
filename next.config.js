3a4a5256d4194230e0570e3d6b687a7eab81da2b
Collin Boler <collinboler@gmail.com>
2025-01-05 14:25:30 -0500

    fix css?

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Remove experimental PPR flag as it requires canary version
+  output: 'standalone',
+  images: {
+    domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
+  },
+  transpilePackages: ['lucide-react'],
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader'],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig; 
